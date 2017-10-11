589ad5fdd75144313d6d8b22756af8ff3a885561
root <root@mars>
2017-10-11 15:58:00 -0500

    joel

--- health/app/admin.py
+++ health/app/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+#EJEMPLOSSSSSSSSSS......
+ 
+from .models import *
+from django.contrib import admin
+from django.contrib.admin.filters import RelatedOnlyFieldListFilter
+
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy
+
+@admin.register(Citas)
+class CitasAdmin(admin.ModelAdmin):
+	list_display = ('nombre','direccion')
+
+
+@admin.register(Pacientes)
+class PacientesAdmin(admin.ModelAdmin):
+	list_display = ('DNI','Domicilio','Ciudad','Telefono','Celular','Email','Referenciado')
+
+
+@admin.register(Habitos)
+class HabitosAdmin(admin.ModelAdmin):
+	list_display = ('nombre',)
+
+
+
+@admin.register(Consulta)
+class ConsultaAdmin(admin.ModelAdmin):
+	list_display = ('nombre',)
+
+
+@admin.register(Tratamiento)
+class TratamientoAdmin(admin.ModelAdmin):
+	list_display = ('Diagnostico','Fecha_ini','Frecuencia','Apoyo','Tipo','Porcentaje','Sesion','Dolor_Fisico','Malestar_Emocional','Estudio_Medico','Firma')
+
